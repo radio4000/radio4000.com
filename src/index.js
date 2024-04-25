d9ec5e8594a47d4506abc01a7ae2035011d68b0f
ug.rp <ugrp@duck.com>
2024-04-25 10:41:03 +0200

    add vite setup

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import "./index.css";
+import "@radio4000/components/style";
+import r4 from "@radio4000/components";
+
+/* App config
+	 - get the public environment config from a json file,
+	 - set it on the app's custom-element
+	 - append the app as the only element of the body's DOM
+ */
+
+const containerElementName = "body";
+const appElementName = "r4-app";
+/* customElements.define(appElementName, r4.R4App) */
+const $app = document.createElement(appElementName);
+fetch("/.env.json")
+	.then(async (data) => {
+		let config = {};
+		try {
+			config = await data.json();
+		} catch (e) {
+			/* console.info('.env.json config file not found') */
+		}
+		return config;
+	})
+	.then((config) => {
+		Object.entries(config).forEach(([key, value]) => {
+			if (typeof value === "object") {
+				$app.setAttribute(key, JSON.stringify(value));
+			} else {
+				$app.setAttribute(key, value);
+			}
+		});
+	})
+	.finally(() => {
+		document.querySelector(containerElementName).append($app);
+		/* console.info(`${appElementName} loaded`) */
+	});
